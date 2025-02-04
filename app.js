0a398de5f2109e63db9141a5decf4fd18f5be38f
Maria Ferreira <maria.ferreira312017@gmail.com>
2025-02-04 20:40:08 +0800

    Adding names

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+let secretFriends = [];
+
+function addFriends(){
+    let inputFriend = document.getElementById('amigo');
+   
+
+    if (inputFriend && inputFriend.value.trim() !== ''){
+        
+        secretFriends.push(inputFriend.value.trim());
+        updateFriendsList();
+        inputFriend.value = '';
+        
+        console.log(secretFriends);
+    
+    }else{
+        alert('Por favor, insira um nome.')
+    }
+
+        
+    }
+    
+    function updateFriendsList(){
+        let friendsList = document.getElementById('listaAmigos');
+        friendsList.innerHTML = '';
+ 
+        for (let i = 0; i < secretFriends.length; i++){
+         const li = document.createElement('li');
+         li.textContent = secretFriends[i];
+         friendsList.appendChild(li);
+ 
+        }
+ 
+       
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
