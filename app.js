406a6a5159834fbe628026db2a9bf15c30d155e4
Maria Ferreira <maria.ferreira312017@gmail.com>
2025-02-09 15:04:43 +0800

    Desafio pronto!

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ function addFriends(){
    
 
     if (inputFriend && inputFriend.value.trim() !== ''){
+        const newFriend = inputFriend.value.trim();
+
+    if (secretFriends.includes(newFriend)){
+        alert('Este nome já foi incluído.');
+            inputFriend.value = ''; // Limpa o campo após o alerta
+            return;
+
+    }
         
-        secretFriends.push(inputFriend.value.trim());
+        secretFriends.push(newFriend);
         updateFriendsList();
         inputFriend.value = '';
         
@@ -31,10 +39,35 @@ function addFriends(){
          friendsList.appendChild(li);
  
         }
- 
-       
+        
     }
 
+    function sortearAmigo() {
+        if (secretFriends.length == 0) {
+            alert('Lista vazia, insira nomes para sortear.');
+            return;
+        }
+
+    const randomIndex = Math.floor(Math.random() * secretFriends.length);
+    const sorteado = secretFriends[randomIndex];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>Amigo secreto sorteado: ${sorteado}</li>`;
+
+    restartGame();
+} 
+
+
+
+function restartGame() {
+    secretFriends = []; 
+    clearField(); 
+}
+
+function clearField() {
+    document.getElementById('listaAmigos').innerHTML = '';
+    
+}
     
 
 
